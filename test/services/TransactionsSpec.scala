a81aced8f8bf75dc4c311620904fdaa760ea0274
Willem Meints <willem@mein-design.nl>
2017-04-01 19:31:35 +0200

    Make the import spec more specific

--- test/services/TransactionsSpec.scala
+++ test/services/TransactionsSpec.scala
@@ -1,21 +1,27 @@
 package services
 
-import java.nio.charset.Charset
-import java.nio.file.{Files, StandardOpenOption}
-
 import helpers.TransactionsFile
-import org.scalacheck.{Prop, Properties}
+import org.scalacheck.Prop
 import org.specs2.ScalaCheck
 import org.specs2.mutable._
 
 class TransactionsSpec extends Specification with ScalaCheck {
-  "Transactions" should {
-    "Correctly process files" in {
-      Prop.forAll(TransactionsFile.sample) { file =>
-        val result = Transactions.parse(file)
-
-        result.isLeft || result.isRight && result.right.get.nonEmpty
-      }
-    }
+  val validFiles = Prop.forAll(TransactionsFile.validFile) { file =>
+    Transactions.parse(file).isRight
   }
+
+  val filesWithMissingFields = Prop.forAll(TransactionsFile.fileWithMissingFieldInRecords) { file =>
+    Transactions.parse(file).isLeft
+  }
+
+  val filesWithoutHeader = Prop.forAll(TransactionsFile.fileWithoutHeader) { file =>
+    Transactions.parse(file).isLeft
+  }
+
+  s2"""
+    The transactions module should be able to import files
+      - It should process valid files $validFiles
+      - It should flag files with missing fields $filesWithMissingFields
+      - It should flag files with an incorrect header $filesWithoutHeader
+    """
 }
