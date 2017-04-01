af817d5e21bc1a3fd86b54f3c461bd21fd26f72f
Willem Meints <willem@mein-design.nl>
2017-04-01 10:40:03 +0200

    Remove event publisher

--- app/modules/ActorsModule.scala
+++ app/modules/ActorsModule.scala
@@ -1,11 +1,10 @@
 package modules
 
-import actors.EventPublisherActor
 import com.google.inject.AbstractModule
 import play.api.libs.concurrent.AkkaGuiceSupport
 
 class ActorsModule extends AbstractModule with AkkaGuiceSupport {
   override def configure(): Unit = {
-    bindActor[EventPublisherActor]("event-publisher")
+
   }
 }
